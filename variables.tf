4a4e67d5f88a8a1b7cf3e3d49d9670e7863623b0
Kaizlr <121697789+Kaizlr@users.noreply.github.com>
2023-02-12 11:23:32 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "us-east-2" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    default     =  "DevopsCoachingHouseKeys" 
 }
 
 variable "instance_type" { 
@@ -15,16 +15,16 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "sg-0460fefdd3b2adb77" 
 }
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
+    default     = "EC2-by-terraform" 
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-0ee23bfc74a881de5" 
+    default     = "ami-071c33e7823c066b5" 
 }
 variable "versioning" {
     type        = bool
@@ -39,7 +39,7 @@ variable "acl" {
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "my-s3bucket-"
+    default     = "my-devops-coaching-bucket"
 }
 variable "tags" {
     type        = map
